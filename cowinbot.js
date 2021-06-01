a71af2acc442e356dcf31aa6f3925b00976681fe
Sneha <nehabphn@gmail.com>
2021-06-01 11:46:43 +0530

    changed api req, better messaging

--- cowinbot.js
+++ cowinbot.js
@@ -5,36 +5,45 @@ const twilio = require('twilio')
 const client = new twilio(twilioSID, twilioToken)
 
 let centerLog = []
-
 // regular logger... but interval
 const getData = async() => {
-    const time = Math.floor(Math.random() * 120000) + (4*60000)
+    const time = Math.floor(Math.random() * 5000) + 60000
     console.log(time/60000)
-    const responseD0 = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${moment().format('DD-MM-YYYY')}`)
-    const responseD1 = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${moment().add(1, 'd').format('DD-MM-YYYY')}`)
-    const responseD2 = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${moment().add(2, 'd').format('DD-MM-YYYY')}`)
-    const combCenters = [...responseD0.data.centers.map(center => ({...center, date: center.date = moment().format('DD-MM-YYYY')})),
-        ...responseD1.data.centers.map(center => ({...center, date: center.date = moment().add(1, 'd').format('DD-MM-YYYY')})),
-        ...responseD2.data.centers.map(center => ({...center, date: center.date = moment().add(2, 'd').format('DD-MM-YYYY')}))]
-    dataLog = combCenters.filter(center => center.fee_type == 'Paid' && center.sessions.find(session => session.min_age_limit == 18 && session.available_capacity_dose1 > 0)).map(center => ({name: center.name, sessions: center.sessions, from: center.from, to: center.to}))
-    centers = combCenters.filter(center => center.fee_type == 'Paid' && center.sessions.find(session => session.min_age_limit == 18)).map(center => ({name: center.name, date: center.date}))
-    newCenters = centers.filter(newCenter => !centerLog.find(oldCenter => oldCenter.name == newCenter.name && oldCenter.date == newCenter.date))
+    const response = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${moment().format('DD-MM-YYYY')}`)
 
-    if(dataLog.length > 0) {
-        console.log(moment().format('DD-MM, hh:mm:ss a'), dataLog, dataLog[0])
+    const centers = response.data.centers.filter(center => center.fee_type == 'Paid' && center.sessions.find(session => session.min_age_limit == 18)).map(center => ({name: center.name, sessions: center.sessions.filter(session => session.min_age_limit == 18).map(session => ({date: session.date, id: session.session_id}))}))
+    
+    const availableCenters = response.data.centers.filter(center => center.fee_type == 'Paid' && center.sessions.find(session => session.min_age_limit == 18 && session.available_capacity_dose1 > 0)).map(center => ({name: center.name, sessions: center.sessions.filter(session => session.min_age_limit ==18), from: center.from, to: center.to}))
+    
+    const newCenters = centers.filter(center => {
+        const oldCenter = centerLog.find(oldCenter => oldCenter.name == center.name)
+        if (!oldCenter) return center
+        else {
+            center.sessions.forEach(session => {
+                if (!oldCenter.sessions.find(oldSession => oldSession.session_id == session.id)) {
+                    return center
+                }
+            })
+        }
+    })
+
+    if(availableCenters.length > 0) {
+        console.log(moment().format('DD-MM, hh:mm:ss a'), availableCenters)
         client.messages
         .create({
            from: 'whatsapp:+14155238886',
-           body: JSON.stringify(dataLog),
+           body: `New Center Alert! ${JSON.stringify(availableCenters.map(center => center.name))}
+            ${JSON.stringify(availableCenters)}`,
            to: `whatsapp:${receiver}`
          })
         .then(message => console.log(message.sid));
     } else if(newCenters.length > 0) {
-        console.log(moment().format('DD-MM, hh:mm:ss a'), centers)
+        console.log(moment().format('DD-MM, hh:mm:ss a'), centers, JSON.stringify(newCenters))
         client.messages
         .create({
            from: 'whatsapp:+14155238886',
-           body: JSON.stringify(centers),
+           body: `New slots were opened, but you missed it 
+           ${JSON.stringify(centers)}`,
            to: `whatsapp:${receiver}`
          })
         .then(message => console.log(message.sid));
